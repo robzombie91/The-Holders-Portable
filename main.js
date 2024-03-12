06b8f818f2b0fb8cc21036e79771659619888ea2
robzombie91 <tankpier222@yahoo.com>
2024-03-11 23:27:54 -0400

    Add files via upload
    added some nice features, 
    search bar now shows a message if items cannot be found, 
    added a hamburger bar for mobile
    some nice little transition effects

--- main.js
+++ main.js
@@ -25,33 +25,34 @@ document.addEventListener('DOMContentLoaded', function() {
   }
   
   function loadContent(contentId) {
-      fetch('loadContentFromServer.php?id=' + contentId)
-          .then(response => {
-              if (!response.ok) { 
-                  throw new Error(response.statusText); 
-              }
-              return response.json();
-          })
-          .then(data => {
-              document.getElementById('main-content').innerHTML = data.content;
-
+    const mainContent = document.getElementById('main-content');
+    mainContent.style.opacity = 0;
   
-              document.querySelector('.navbar-brand').textContent = data.title;
+    fetch('loadContentFromServer.php?id=' + contentId)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        return response.json();
+      })
+      .then(data => {
+        mainContent.innerHTML = data.content;
+        
+        setTimeout(() => {
+          mainContent.style.transition = 'opacity 0.5s';
+          mainContent.style.opacity = 1;
+        }, 100);
   
-              
-    // Scroll to the top of the page
-    window.scrollTo(0, 0);
-    // Clear the search box
-    searchBox.value = '';
-
-
-    // Add more functionality if needed
-    
-          })
-          .catch(error => {
-              console.error("Error fetching content: ", error);
-              document.getElementById('main-content').innerHTML = "Error loading content. Please try again later.";
-          });
+        document.querySelector('.navbar-brand').textContent = data.title;
+  
+        window.scrollTo(0, 0);
+        searchBox.value = '';
+      })
+      .catch(error => {
+        console.error("Error fetching content: ", error);
+        mainContent.innerHTML = "Error loading content. Please try again later.";
+        mainContent.style.opacity = 1;
+      });
   }
 
 // Typeahead Functionality
